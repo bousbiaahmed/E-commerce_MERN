6b48d27197385218080acef77e82b8da6f917547
Ahmed <bousbiaa213@gmail.com>
2025-04-18 01:03:53 +0200

    initial Commit + user Ferture

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRoute from "./routes/userRoute";
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/ecommerce")
+  .then(() => {
+    console.log("Mongo connected");
+  })
+  .catch((err) => {
+    console.log("failed to connect",err);
+  });
+
+
+app.use('/user',userRoute)
+
+
+app.listen(port,()=>{
+    console.log('server is running at:http://localhost:3001')
+})
